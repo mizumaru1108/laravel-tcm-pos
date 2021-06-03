eee280fc1c052949b2469be8130902b37eaff143
LilRookie69 <ridhoiyoy962013@gmail.com>
2021-06-03 21:25:28 +0700

    finishing login feature

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@tcm.com',
+            'password' => bcrypt('admin123')
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Category\CategoryController;
+use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('menu', MenuController::class);
+    Route::get('auth', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::get('profile', [AuthController::class, 'profile'])->name('auth.profile');
     Route::resource('user', UserController::class);
+    Route::resource('category', CategoryController::class);
 });
 
 Route::post('auth', [AuthController::class, 'login'])->name('auth.login');
